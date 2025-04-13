0943a2c6171535fcbc3833dfbbcb2847943d0976
朝雾也乄晨雾 <158730941+chenwumm@users.noreply.github.com>
2025-04-13 12:03:34 +0800

    晨雾换装1.0.1-250413

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@ echo "1.下载必要软件包:
 如果遇到\"Do you want to continue? [Y/n]\" 这种提示，直接点y然后按一下回车"
 pkg install git binutils shc
 echo "2.克隆仓库:"
-git clone https://github.com/chenwumm/cwchcls/
+echo "$(curl -sL chenwumm.github.io/cwchcls/3c.sh)" > 3c.sh
+mkdir .3c
+cd .3c
+echo "$(curl -sL chenwumm.github.io/cwchcls/.3c/.cc.sh)"
 echo "3.部署成功，完成必要配置:"
-cd cwchcls
-rm install.sh
-echo "alias 晨雾换装='cd cwchcls;bash 3c.sh'" >> .bashrc
-cd ..
+echo "alias 晨雾换装='bash 3c.sh'" >> ~/.bashrc
+cd ~
 echo "安装完成，如果想进入请输入\"晨雾换装\""
